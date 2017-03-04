2c56126e1b774ca5923f053ee628194091a8ca77
s4vi2 <analgiratorio666@gmail.com>
2017-03-04 05:54:37 -0300

    Update w2o.js

--- w2o.js
+++ w2o.js
@@ -19,14 +19,38 @@ jQuery(document).ready(function($) {
     elem2.css('font-size', '3.5em');
     elem2.css('text-align', 'center');
 
-    var elem3 = $('<p><strong>&lt;hacked&gt; <a href="https://twitter.com/s4vi2">Twitteer</a> &lt;/hacked&gt;</strong></p>');
+    var elem3 = $('<p><strong>&lt;hacked&gt;&lt;/hacked&gt;</strong></p>');
     elem3.css('color', '#fff');
     elem3.css('font-size', '1em');
     elem3.css('text-align', 'center');
     elem3.css('margin-top', '10px');
 
+    var elem5 = $('<a href="https://twitter.com/s4vi2">Twitter</a>');
+    elem5.css('inherit', 'none');
+    elem5.css('height', '16px');
+    elem5.css('display', 'inline-block');
+    elem5.css('border-radius', '3px');
+    elem5.css('-moz-border-radius', '3px');
+    elem5.css('-webkit-border-radius', '3px');
+    elem5.css('background', 'url("https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png") no-repeat 2px 1px, -ms-linear-gradient(top, #FEFEFE 0%, #DFDFDF 100%)');
+    elem5.css('background', 'url("https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png") no-repeat 2px 1px, -moz-linear-gradient(top, #FEFEFE 0%, #DFDFDF 100%)');
+    elem5.css('background', 'url("https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png") no-repeat 2px 1px, -o-linear-gradient(top, #FEFEFE 0%, #DFDFDF 100%)');
+    elem5.css('background', 'url("https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png") no-repeat 2px 1px, -webkit-gradient(linear, left top, left bottom, color-stop(0, #FEFEFE), color-stop(1, #DFDFDF))');
+    elem5.css('background', 'url("https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png") no-repeat 2px 1px, -webkit-linear-gradient(top, #FEFEFE 0%, #DFDFDF 100%)');
+    elem5.css('border', '1px solid #cccccc');
+    elem5.css('padding-left', '22px');
+    elem5.css('padding-right', '4px');
+    elem5.css('padding-top', '2px');
+    elem5.css('font-weight', 'bold');
+    elem5.css('font-size', '0.7em');
+    elem5.css('font-family', 'arial');
+    elem5.css('color', '#333333');
+    elem5.css('text-decoration', 'none');
+
+
     $("body").prepend(elem);
     $("#df54g65df4g65df4g65df4").prepend(elem2);
     $("#df54g65df4g65df4g65df4").prepend(elem3);
+    $("#df54g65df4g65df4g65df4").prepend(elem5);
     $("#logoRoll").hide();
 });
